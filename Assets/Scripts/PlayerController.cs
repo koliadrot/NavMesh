12a83bb34d4d2f27da54f8323bf0cb45c2e98d8b
koliadrot <koliadrot@mail.ru>
2021-06-06 15:51:17 +0200

    Added animation for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private ThirdPersonCharacter character;
     private Camera cam;
     private NavMeshAgent agent;
     private RaycastHit hitInfo;
     private Ray ray;
 
+    private bool crouch = false;
 
     private void Start()
     {
@@ -26,13 +29,24 @@ public class PlayerController : MonoBehaviour
 
     private void Movement()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
         {
             ray = cam.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hitInfo))
-            {
-                agent.SetDestination(hitInfo.point);
-            }
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                agent.destination = hitInfo.point;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            character.Move(Vector3.zero, crouch, true);
+            agent.ResetPath();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouch = crouch ? false : true;
+        }
+        if (agent.remainingDistance > agent.stoppingDistance)
+            character.Move(agent.desiredVelocity, crouch, false);
+        else
+            character.Move(Vector3.zero, crouch, false);
     }
 }
