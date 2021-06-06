d846cb9e6f466437bdd7ea9f1157b49fc4158234
koliadrot <koliadrot@mail.ru>
2021-06-06 14:16:17 +0200

    Added control system of player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    private NavMeshAgent agent;
+    private RaycastHit hitInfo;
+    private Ray ray;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
